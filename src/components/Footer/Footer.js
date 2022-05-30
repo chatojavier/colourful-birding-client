e6005d9c00ea5dbde3eba1f20db309eb428b977c
Javier Benavides <chato.javier@gmail.com>
2022-05-30 11:04:19 -0500

    fix menu locations bug

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,15 @@
-// import useSite from 'hooks/use-site';
+import useSite from 'hooks/use-site';
 import Container from 'components/Container';
 import Image from 'next/image';
 import Divider from 'components/Divider';
 import styles from './Footer.module.scss';
+import { findMenuByLocation } from 'lib/menus';
+import Link from 'next/link';
 
 const Footer = () => {
-  // const { metadata = {}, recentPosts = [], categories = [], menus } = useSite();
-  // const { title } = metadata;
+  const { menus } = useSite();
 
-  // const navigation = findMenuByLocation(menus, [process.env.WORDPRESS_MENU_LOCATION_NAVIGATION]);
-  // console.log(navigation);
+  const navigation = findMenuByLocation(menus, [process.env.WORDPRESS_MENU_ALTERNATIVE_NAVIGATION]);
 
   return (
     <footer>
@@ -18,13 +18,19 @@ const Footer = () => {
       </section>
       <Divider />
       <section className="footer | container relative mx-auto p-4">
-        <ul
-          className={`footer__menu | mt-2 mb-8 flex w-full flex-col items-center space-y-4 text-xxs uppercase text-blue md:flex-row md:justify-center md:space-y-0 md:space-x-4 md:text-sm lg:absolute ${styles.menu}`}
-        >
-          <li>Information & Local Regulations</li>
-          <li>Privacy Policies</li>
-          <li>Terms & Conditions</li>
-        </ul>
+        {navigation && (
+          <ul
+            className={`footer__menu | mt-2 mb-8 flex w-full flex-col items-center space-y-4 text-xxs uppercase text-blue md:flex-row md:justify-center md:space-y-0 md:space-x-4 md:text-xs lg:absolute ${styles.menu}`}
+          >
+            {navigation?.map((menuItem) => (
+              <li key={menuItem.label} className="footer__menu-item | flex items-center">
+                <Link href={menuItem.path}>
+                  <a>{menuItem.label}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
         <div className="flex flex-col items-center md:flex-row md:items-end md:justify-between md:space-x-8">
           <div className="brandby-wrapper mb-8 md:mb-0 md:shrink-0">
             <p className="text-sm">A brand of:</p>
