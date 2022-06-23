330a287fc586f23d23f31426b8495730cb1e51ff
Javier Benavides <chato.javier@gmail.com>
2022-06-23 14:37:42 -0500

    Develop article content

---
+++ src/components/ArticleContent/ArticleContent.js
@@ -0,0 +1,38 @@
+import DateFormated from 'components/DateFormated';
+import Divider from 'components/Divider';
+
+const { default: FormatedContent } = require('components/FormatedContent');
+
+const ArticleContent = ({ children, author, date }) => {
+  const ArticleHeader = ({ className }) => (
+    <div className={`article-header |  flex items-end justify-between ${className}`}>
+      <div className="article-author | flex items-end space-x-2">
+        <div className="article-author__image | h-12 w-12 border p-1">
+          <img
+            src={author.avatar.url}
+            alt={author.name}
+            width={author.avatar.width}
+            height={author.avatar.height}
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="article-author__info | text-xs md:text-sm">
+          <div className="">Author</div>
+          <div className="uppercase">{author.name}</div>
+        </div>
+      </div>
+      <div className="article-date">
+        <DateFormated date={date} />
+      </div>
+    </div>
+  );
+  return (
+    <div className="article-content | text-xs md:text-sm">
+      <ArticleHeader className="mb-12 md:mb-20" />
+      <FormatedContent content={children} className="mb-8 md:mb-12" />
+      <Divider />
+    </div>
+  );
+};
+
+export default ArticleContent;
---
+++ src/components/ArticleContent/ArticleContent.stories.js
@@ -0,0 +1,108 @@
+import Container from 'components/Container';
+import ArticleContent from './ArticleContent';
+
+export default {
+  name: 'components/ArticleContent',
+  component: ArticleContent,
+};
+
+const post = {
+  __typename: 'Post',
+  author: {
+    __typename: 'User',
+    avatar: {
+      __typename: 'Avatar',
+      height: 96,
+      url: 'https://secure.gravatar.com/avatar/c2b06ae950033b392998ada50767b50e?s=96&d=mm&r=g',
+      width: 96,
+    },
+    id: 'dXNlcjox',
+    name: 'colourful',
+    slug: 'colourful',
+  },
+  id: 'cG9zdDoxOTg=',
+  categories: [],
+  content:
+    '\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam phasellus vestibulum lorem. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Convallis aenean et tortor at risus viverra. Nisl pretium fusce id velit ut. Augue lacus viverra vitae congue eu consequat ac felis. Vitae sapien pellentesque habitant morbi. Et odio pellentesque diam volutpat commodo. Pulvinar pellentesque habitant morbi tristique. Vitae nunc sed velit dignissim sodales ut eu. Nullam ac tortor vitae purus faucibus ornare suspendisse. Condimentum id venenatis a condimentum vitae sapien pellentesque. Lorem dolor sed viverra ipsum nunc. Neque aliquam vestibulum morbi blandit. Rhoncus aenean vel elit scelerisque mauris. Cum sociis natoque penatibus et magnis. Sed faucibus turpis in eu. Id faucibus nisl tincidunt eget nullam non.</p>\n\n\n\n<p>Mauris nunc congue nisi vitae suscipit tellus mauris. Senectus et netus et malesuada fames. Erat imperdiet sed euismod nisi porta. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Nam libero justo laoreet sit amet cursus sit. Adipiscing elit duis tristique sollicitudin nibh sit amet. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Tellus molestie nunc non blandit. Sit amet purus gravida quis. Pulvinar neque laoreet suspendisse interdum consectetur libero id. Eu mi bibendum neque egestas congue. Eu consequat ac felis donec et odio pellentesque diam volutpat.</p>\n\n\n\n<p>Eget duis at tellus at urna condimentum mattis pellentesque. Condimentum id venenatis a condimentum. Ut tellus elementum sagittis vitae et leo. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tincidunt arcu non sodales neque sodales. Magna eget est lorem ipsum dolor sit amet consectetur. Euismod in pellentesque massa placerat duis ultricies lacus sed. Arcu dui vivamus arcu felis bibendum. Purus non enim praesent elementum facilisis leo vel fringilla est. Turpis nunc eget lorem dolor. Purus ut faucibus pulvinar elementum integer enim neque. Nisi est sit amet facilisis magna etiam. Turpis massa tincidunt dui ut ornare lectus sit amet. Duis tristique sollicitudin nibh sit. Morbi tincidunt ornare massa eget egestas purus viverra. Massa enim nec dui nunc mattis enim ut tellus. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Sit amet purus gravida quis blandit turpis cursus in. Diam donec adipiscing tristique risus. Massa tincidunt nunc pulvinar sapien.</p>\n\n\n\n<figure class="wp-block-image size-large"><img loading="lazy" width="1024" height="768" src="https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=1024%2C768&#038;ssl=1" alt="" class="wp-image-187" srcset="https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=1024%2C768&amp;ssl=1 1024w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=300%2C225&amp;ssl=1 300w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=768%2C576&amp;ssl=1 768w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=1536%2C1152&amp;ssl=1 1536w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/06/Lago_Sandoval_4.jpg?resize=2048%2C1536&amp;ssl=1 2048w" sizes="(max-width: 1000px) 100vw, 1000px" data-recalc-dims="1" /></figure>\n\n\n\n<p>Morbi tincidunt ornare massa eget egestas purus. Molestie at elementum eu facilisis sed. Porttitor rhoncus dolor purus non enim praesent elementum facilisis leo. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Tincidunt dui ut ornare lectus sit amet est placerat in. Facilisis magna etiam tempor orci eu lobortis. Sit amet facilisis magna etiam tempor orci eu lobortis. Nulla facilisi nullam vehicula ipsum. Id neque aliquam vestibulum morbi. Ac ut consequat semper viverra nam libero justo laoreet. Amet commodo nulla facilisi nullam vehicula ipsum a. Cursus euismod quis viverra nibh cras pulvinar mattis. Parturient montes nascetur ridiculus mus mauris vitae ultricies.</p>\n\n\n\n<p>Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Lobortis feugiat vivamus at augue eget arcu. Velit scelerisque in dictum non consectetur. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Tellus cras adipiscing enim eu turpis. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Viverra nibh cras pulvinar mattis nunc. Aliquam id diam maecenas ultricies mi eget. Rhoncus aenean vel elit scelerisque mauris. Ultrices tincidunt arcu non sodales neque sodales ut.</p>\n\n\n\n<p>At risus viverra adipiscing at in tellus integer feugiat scelerisque. Ut tellus elementum sagittis vitae et leo. Eu facilisis sed odio morbi. Turpis cursus in hac habitasse platea dictumst. Erat velit scelerisque in dictum non. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Id eu nisl nunc mi. Quam quisque id diam vel quam. Nulla pellentesque dignissim enim sit. Mi bibendum neque egestas congue quisque. Enim nulla aliquet porttitor lacus. Mauris cursus mattis molestie a iaculis at erat. Vel turpis nunc eget lorem dolor sed viverra ipsum nunc. Ut etiam sit amet nisl purus in mollis. Convallis convallis tellus id interdum. Tincidunt eget nullam non nisi est sit amet facilisis. Nunc sed id semper risus. Quam nulla porttitor massa id neque aliquam vestibulum. Congue nisi vitae suscipit tellus mauris.</p>\n\n\n\n<p>Commodo ullamcorper a lacus vestibulum. Quis blandit turpis cursus in hac. Bibendum at varius vel pharetra. Auctor augue mauris augue neque gravida in fermentum. Morbi tempus iaculis urna id volutpat. Cursus risus at ultrices mi tempus imperdiet. Diam in arcu cursus euismod quis viverra nibh. Egestas dui id ornare arcu odio. Scelerisque felis imperdiet proin fermentum. In tellus integer feugiat scelerisque varius.</p>\n\n\n\n<p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Tellus at urna condimentum mattis. Dui vivamus arcu felis bibendum ut tristique et egestas quis. Et malesuada fames ac turpis egestas sed tempus urna et. Dolor morbi non arcu risus. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Viverra justo nec ultrices dui sapien eget mi. Id diam maecenas ultricies mi eget mauris pharetra. Lacus vel facilisis volutpat est. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Sapien et ligula ullamcorper malesuada proin libero nunc consequat. Eget dolor morbi non arcu risus quis varius quam quisque. Dictum non consectetur a erat. Diam donec adipiscing tristique risus nec feugiat in. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Blandit aliquam etiam erat velit scelerisque in. Massa tempor nec feugiat nisl pretium fusce id velit. Pretium quam vulputate dignissim suspendisse in est. Aliquet lectus proin nibh nisl condimentum id. Dui ut ornare lectus sit amet est placerat in.</p>\n',
+  date: '2022-06-22T14:43:27',
+  excerpt:
+    '<p>Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tincidunt arcu non sodales neque sodales. Magna eget est lorem ipsum dolor sit amet consectetur.</p>\n',
+  featuredImage: {
+    __typename: 'MediaItem',
+    altText: '',
+    caption: null,
+    sourceUrl:
+      'https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/05/1800-4-1.jpeg?fit=863%2C647&ssl=1',
+    srcSet:
+      'https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/05/1800-4-1.jpeg?w=863&ssl=1 863w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/05/1800-4-1.jpeg?resize=300%2C225&ssl=1 300w, https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/05/1800-4-1.jpeg?resize=768%2C576&ssl=1 768w',
+    sizes: '(max-width: 300px) 100vw, 300px',
+    id: 'cG9zdDo1Mw==',
+  },
+  modified: '2022-06-22T14:43:27',
+  databaseId: 198,
+  title: 'Diam donec adipiscing tristique',
+  slug: 'diam-donec-adipiscing-tristique',
+  isSticky: false,
+  contentTypeName: 'post',
+  metaTitle: 'Diam donec adipiscing tristique - Colourful Birding',
+  metaDescription: '',
+  readingTime: 5,
+  og: {
+    author: '',
+    description:
+      'Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tincidunt arcu non sodales neque sodales. Magna eget est lorem ipsum dolor sit amet consectetur.',
+    image: {
+      __typename: 'MediaItem',
+      altText: '',
+      sourceUrl:
+        'https://i0.wp.com/admin.colourfulbirding.com/wp-content/uploads/2022/05/Colourful-birding-logo-08-scaled.jpg?fit=2560%2C1440&ssl=1',
+      mediaDetails: {
+        __typename: 'MediaDetails',
+        height: 1440,
+        width: 2560,
+      },
+    },
+    modifiedTime: '',
+    publishedTime: '2022-06-22T19:43:27+00:00',
+    publisher: 'https://facebook.com/colourfulperu',
+    title: 'Hello World! | Colourful Birding',
+    type: 'article',
+    imageUrl: 'https://colourfulbirding.com/images/og/diam-donec-adipiscing-tristique.png',
+    imageSecureUrl: 'https://colourfulbirding.com/images/og/diam-donec-adipiscing-tristique.png',
+    imageWidth: 2000,
+    imageHeight: 1000,
+  },
+  article: {
+    author: '',
+    modifiedTime: '',
+    publishedTime: '2022-06-22T19:43:27+00:00',
+    publisher: 'https://facebook.com/colourfulperu',
+  },
+  robots: {
+    nofollow: 'follow',
+    noindex: 'index',
+  },
+  twitter: {
+    description: '',
+    image: {
+      __typename: 'MediaItem',
+      altText: null,
+      sourceUrl: null,
+      mediaDetails: null,
+    },
+    title: '',
+  },
+};
+
+const Template = (args) => (
+  <Container>
+    <ArticleContent {...args} />
+  </Container>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: post.content,
+  author: post.author,
+  date: post.date,
+};
