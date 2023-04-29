ddf183b9736a7c6ec35b45043378d5010edac6dd
Javier Benavides <javier.benavides01@globant.com>
2023-04-28 20:37:17 -0500

    fixing date difference in days

--- src/components/DateFromTo/DateFromTo.js
+++ src/components/DateFromTo/DateFromTo.js
@@ -8,13 +8,14 @@ const DateFromTo = ({ from, to }) => {
   const dateFromPattern = dateFrom.getFullYear() === dateTo.getFullYear() ? 'MMM d' : 'MMM d, yyyy';
   const dateFromFormatted = format(dateFrom, dateFromPattern);
   const dateToFormatted = format(dateTo, 'MMM d, yyyy');
-  const daysOfDifference = Math.abs(dateTo.getDate() - dateFrom.getDate() + 1);
-  const daysOfDifferenceFormatted = daysOfDifference === 1 ? '1 day' : `${daysOfDifference} days`;
+  const differenceInTime = dateTo.getTime() - dateFrom.getTime();
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24) + 1;
+  const differenceInDaysFormatted = differenceInDays === 1 ? '1 day' : `${differenceInDays} days`;
   return (
     <span>
       <span className="inline-block">{dateFromFormatted}</span>
       <span className="inline-block">{dateFromFormatted !== dateToFormatted && <>&nbsp;- {dateToFormatted}</>}</span>
-      <span className="inline-block">&nbsp;{'(' + daysOfDifferenceFormatted + ')'}</span>
+      <span className="inline-block">&nbsp;{'(' + differenceInDaysFormatted + ')'}</span>
     </span>
   );
 };
